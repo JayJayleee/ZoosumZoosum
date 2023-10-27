60d42d135b2db18eb936968adee44b1aaee7e1e3
노수혁 <roh403@naver.com>
2023-10-27 15:07:28 +0900

    Feat: 스타일 적용을 위해 style 파일 import

--- frontend/src/pages/LoginPage/index.tsx
+++ frontend/src/pages/LoginPage/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, Image, TouchableOpacity} from 'react-native';
 import { LoginScreenProps } from '@/types/path';
+import styles from './style';
 
 export default function LoginPage({navigation}: LoginScreenProps) {
   return (
-  <TouchableOpacity 
+  <TouchableOpacity
+    style={styles.container}
     onPress={() => navigation.navigate('Main')}>
     <View>
       <Image 
