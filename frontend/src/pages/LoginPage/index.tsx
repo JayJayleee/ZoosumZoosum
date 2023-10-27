611340e637aef292aa3f4c540bbc19f579e00403
노수혁 <roh403@naver.com>
2023-10-27 14:55:32 +0900

    Feat: 로그인 페이지 로고 삽입 및 화면 클릭 시 메인 페이지로 이동하도록 navigate 설정

--- frontend/src/pages/LoginPage/index.tsx
+++ frontend/src/pages/LoginPage/index.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Image, TouchableOpacity} from 'react-native';
+import { LoginScreenProps } from '@/types/path';
 
-export default function LoginPage() {
-  return <View>index</View>;
+export default function LoginPage({navigation}: LoginScreenProps) {
+  return (
+  <TouchableOpacity 
+    onPress={() => navigation.navigate('Main')}>
+    <View>
+      <Image 
+        source={require('../../assets/zooisland_logo.png')}
+      />
+    </View>
+  </TouchableOpacity>
+  );
 }
