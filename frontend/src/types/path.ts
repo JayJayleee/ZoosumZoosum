365581c28a343955d0e5f044a7de48c13c3297c8
김영민 <ghg303@naver.com>
2023-10-30 17:50:36 +0900

    Merge: path.ts 충돌 오류 해결

--- frontend/src/types/path.ts
+++ frontend/src/types/path.ts
@@ -40,14 +40,11 @@ export type PloggingResultscreenProps = NativeStackScreenProps<
   'PloggingResult'
 >;
 
-<<<<<<< frontend/src/types/path.ts
 export type FriendListscreenProps = NativeStackScreenProps<
   RootStackParamList,
   'FriendList'
 >;
-=======
 export type UserInforscreenProps = NativeStackScreenProps<
   RootStackParamList,
   'UserInfo'
 >
->>>>>>> frontend/src/types/path.ts
