10bf51478a676bc2b146dd4377a18d6559eb6aa1
노수혁 <roh403@naver.com>
2023-10-31 09:35:37 +0900

    Feat: AsyncStorage 기능과 header 모듈화, api는 추후 개발

--- frontend/src/apis/index.ts
+++ frontend/src/apis/index.ts
@@ -1,6 +1,29 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default function index() {
+// storage에 저장된 token 가져오기(get)
+export const getStorage = async () => {
+  const Result = await AsyncStorage.getItem("AccessToken");
+  return Result;
+}
 
+// storage에 받은 token 저장하기(set)
+export const setStorage = async (token:string) => {
+  return await AsyncStorage.setItem("AccessToken", token);
+}
+
+// storage에 저장된 token 삭제하기(delete)
+export const removeStorage = async () => {
+  return await AsyncStorage.removeItem("AccessToken");
+}
+
+// api header
+export const Header = () => ({
+  headers: {
+    Authorization: `Bearer ${getStorage()}`,
+  },
+});
+
+// api
+export const api = {
+  
 }
