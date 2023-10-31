25a2b4ebeab94bfccfeafe96cb689ca3922377b6
김영민 <ghg303@naver.com>
2023-10-31 10:47:54 +0900

    Feat: 동물 선택 페이지 퍼블리싱 완료

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -24,6 +24,7 @@ import PloggingResultPage from './src/pages/PloggingResultPage';
 import UserInfoPage from '@/pages/UserInfoPage';
 
 import FriendListPage from '@/pages/FriendListPage';
+import PickFriendPage from '@/pages/PickFriendPage';
 
 // 여기서는 RootStackParamList 안에 있는 타입 지정 안해주면 에러남~!꼭 넣을 것
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -44,6 +45,7 @@ function App() {
         <Stack.Screen name="Camera" component={CameraPage} />
         <Stack.Screen name="FriendList" component={FriendListPage} />
         <Stack.Screen name="UserInfo" component={UserInfoPage} />
+        <Stack.Screen name="PickFriend" component={PickFriendPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ frontend/src/pages/PickFriendPage/style.ts
@@ -0,0 +1,54 @@
+import { StyleSheet, Button } from 'react-native';
+
+const styles = StyleSheet.create({
+  backgroungcolor : {
+    backgroundColor: 'black',
+    position: 'absolute',
+    width : '100%',
+    height : '100%',
+    opacity : 0.2,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  title_head : {
+    color : 'white',
+    fontSize : 35,
+  },
+  head : {
+    height : '20%',
+    marginTop : '10%',
+    justifyContent : 'center',
+    alignItems : 'center',
+  },
+  body1 : {
+    height : '80%',
+    width : '100%',
+    alignItems : 'center',
+    marginBottom : 30,
+  },
+  having_cardlist : {
+    width : '85%',
+    height : '65%',
+    marginTop : 10,
+  },
+  button : {
+    width : 200,
+    height : 50,
+    flex : 1,
+    justifyContent : 'center',
+    alignItems : 'center',
+    backgroundColor : '#34D399',
+    borderRadius : 15,
+    position : 'absolute',
+    top : '85%',
+  },
+  button_text : {
+    color : 'white',
+    fontSize : 18,
+    fontWeight : "600"
+  },
+});
+
+export default styles;
