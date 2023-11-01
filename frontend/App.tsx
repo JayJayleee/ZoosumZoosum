ba354abc66b03b498b9524cbb96080965721f85b
노수혁 <roh403@naver.com>
2023-11-01 09:44:31 +0900

    Delete: 불필요한 주석 구문 삭제 처리

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -17,11 +17,7 @@ import PloggingPage from '@/pages/PloggingPage';
 import CameraPage from '@/pages/CameraPage';
 import LoginPage from '@/pages/LoginPage';
 
-// import MainPage from './src/pages/MainPage';
-// import PloggingPage from './src/pages/PloggingPage';
-// import CameraPage from './src/pages/CameraPage';
-// import LoginPage from './src/pages/LoginPage';
-import PloggingResultPage from './src/pages/PloggingResultPage';
+import PloggingResultPage from '@/pages/PloggingResultPage';
 import UserInfoPage from '@/pages/UserInfoPage';
 
 import FriendListPage from '@/pages/FriendListPage';
