ab23898da6cfdf37c37171831e50017d16f2dc00
노수혁 <roh403@naver.com>
2023-11-03 02:23:47 +0900

    Feat: 첫 번째 로그인과 닉네임/지역 설정 후 토큰 받은 api 구현

--- frontend/src/apis/login.ts
+++ frontend/src/apis/login.ts
@@ -6,6 +6,15 @@ type sendAccountContent = {
   socialType: string;
 }
 
+type sendUserInfoContent = {
+  nickname: string;
+  region: string;
+}
+
 export const loginFtn = async (data: sendAccountContent) => {
   return await api.post("/user/login", data);
+}
+
+export const setUserInfoFtn = async (data: sendUserInfoContent) => {
+  return await api.put("/user/info", data, await Header())
 }
