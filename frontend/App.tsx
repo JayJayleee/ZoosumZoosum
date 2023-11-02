5de5cf9dc4b90abc94244f23675e72323cef6b06
노수혁 <roh403@naver.com>
2023-11-03 02:25:25 +0900

    Feat: 첫 페이지 로그인 페이지로 변경

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -47,7 +47,7 @@ function App() {
       <NavigationContainer>
         {/* initialRouteName 는 가장 처음 나타나는 화면을 의미한다 */}
         <Stack.Navigator
-          initialRouteName="FriendDetail"
+          initialRouteName="Login"
           // 아래 코드 넣으면 뒤로가기 바가 있는 헤더가 사라짐
           screenOptions={{headerShown: false}}
         >
