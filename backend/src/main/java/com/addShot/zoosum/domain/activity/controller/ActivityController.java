7b6d27135f7998c19d7239cd2394f9962f45f286
송병훈 <thdqudgns9612@gmail.com>
2023-11-05 13:19:30 +0900

    Update backend/src/main/java/com/addShot/zoosum/domain/activity/controller/ActivityController.java, backend/src/main/java/com/addShot/zoosum/util/s3/S3Service.java

--- backend/src/main/java/com/addShot/zoosum/domain/activity/controller/ActivityController.java
+++ backend/src/main/java/com/addShot/zoosum/domain/activity/controller/ActivityController.java
@@ -89,7 +89,7 @@ public class ActivityController {
         description = "플로깅 정보를 입력하는 동시에, 리워드를 제공")
     @PostMapping("/{userId}")
     public ResponseEntity<?> writeActivity(@PathVariable(name = "userId") String userId,
-        @RequestPart(name = "activityImg") MultipartFile activityImg,
+        @RequestPart(name = "activityImg", required = false) MultipartFile activityImg,
         @RequestPart(name = "activityRequestDto") ActivityRequestDto activityRequestDto){
         if (userId == null || activityRequestDto == null || activityImg == null) {
             return badRequest400();
