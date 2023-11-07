d4f1081561792a878f7a77d32e1c064a75f5c20f
노수혁 <roh403@naver.com>
2023-11-07 14:06:24 +0900

    Feat: 모달 창 내부 버튼 appbutton으로 변경

--- frontend/src/components/ui/Modal/styles.ts
+++ frontend/src/components/ui/Modal/styles.ts
@@ -3,6 +3,8 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
+    width: '100%',
+    height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'rgba(0,0,0,0.5)',
@@ -15,9 +17,12 @@ const styles = StyleSheet.create({
   },
   modalView: {
     width: '80%',
+    height: '40%',
     backgroundColor: 'white',
     borderRadius: 10,
-    padding: 15,
+    padding: '2%',
+    paddingTop: '20%',
+    justifyContent: 'center',
     alignItems: 'center',
     shadowColor: '#000',
     shadowOffset: {
