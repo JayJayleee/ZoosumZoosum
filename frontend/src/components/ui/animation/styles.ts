b66b48afb025d9608716ed4849394d426736f149
노수혁 <roh403@naver.com>
2023-11-09 02:22:55 +0900

    Style: 프로필에서 로딩창 제거로 인해 크기 조정

--- frontend/src/components/ui/animation/styles.ts
+++ frontend/src/components/ui/animation/styles.ts
@@ -5,7 +5,7 @@ import { windowWidth, windowHeight } from "@/constants/styles";
 export const styles = StyleSheet.create({
   wave: {
     width: windowWidth * 0.9, 
-    bottom:  windowHeight * 0.3,
+    bottom:  windowHeight * 0.1,
     aspectRatio: 1
   },
 })
