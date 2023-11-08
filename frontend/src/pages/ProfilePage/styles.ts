b76a8600acffecd351b94185bfde6db48abd3133
노수혁 <roh403@naver.com>
2023-11-08 12:59:37 +0900

    Style: 프로필 페이지 탭 부분 css 추가

--- frontend/src/pages/ProfilePage/styles.ts
+++ frontend/src/pages/ProfilePage/styles.ts
@@ -4,4 +4,25 @@ export const styles = StyleSheet.create({
   container: {
 
   },
+  tabSection: {
+    flexDirection: 'row', 
+    backgroundColor: '#A5DCA0', 
+    borderRadius: 15, 
+    width: '95%', 
+    height: '11%',
+    paddingBottom: 10,
+    justifyContent: 'space-evenly', 
+    alignItems: 'center'
+  },
+  tabClickSection : {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  tabImage: {
+    width: '80%', 
+    height: '80%'
+  },
+  tabText: {
+    fontSize: 15
+  },
 })
