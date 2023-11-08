01377e872489f8d523c572f4530b297bc2a99674
노수혁 <roh403@naver.com>
2023-11-08 14:23:15 +0900

    Style: 인라인 CSS 분리 및 내부 텍스트 변경

--- frontend/src/pages/ProfilePage/StatisticTab.tsx
+++ frontend/src/pages/ProfilePage/StatisticTab.tsx
@@ -2,6 +2,7 @@ import React, { View, ImageBackground } from "react-native"
 import FastImage from "react-native-fast-image"
 import AppText from "@/components/ui/Text"
 import { statisticInfo } from "@/types/profile"
+import { styles } from "./styles"
 
 type statisticProps = {
   statisticList: statisticInfo;
@@ -12,61 +13,53 @@ type statisticProps = {
 export default function StatisticTab({nickname, isMyProfile, statisticList}: statisticProps) {
   return (
   <>
-    <AppText style={{fontFamily: 'NPSfont_bold',fontSize: 40, top:30, color: 'white', justifyContent:'center', textAlign: 'center'}} >
+    <AppText style={styles.staTitle} >
       {isMyProfile? "나의 산책 기록" :`${nickname}님의 산책 기록`}
     </AppText>
-    <ImageBackground source={require("@/assets/profile_image/profile_recipe.png")} style={{width: 400, height: 650, top:30, justifyContent: 'center', alignItems: 'center'}}>
-      <View style={{width: 300, height: 130, top: 25, flexDirection:'row',justifyContent:'space-evenly', alignItems: 'center'}}>
+    <ImageBackground source={require("@/assets/profile_image/profile_recipe.png")} style={styles.staNoteImg}>
+      <View style={styles.staNoteSection}>
+        <FastImage source={require("@/assets/img_icon/foot_icon.png")} style={styles.staNoteIconFoot}/>
         <View>
-          <FastImage source={require("@/assets/img_icon/foot_icon.png")} style={{width:80, height:80}}/>
+          <AppText style={styles.staNoteTitle}>
+            플로깅 횟수
+          </AppText>
+          <AppText style={styles.staNoteContent}>
+            {statisticList.plogCount}회
+          </AppText>
         </View>
+      </View>
+      <View style={styles.staNoteLine}/>
+      <View style={styles.staNoteSection}>
+        <FastImage source={require("@/assets/img_icon/shoe_icon.png")} style={styles.staNoteIcon}/>
         <View>
-          <AppText style={{fontFamily: 'NPSfont_bold',fontSize: 30, color: 'black'}}>
+          <AppText style={styles.staNoteTitle}>
             이동한 거리
           </AppText>
-          <AppText style={{fontFamily: 'NPSfont_bold',fontSize: 20, color: 'black'}}>
+          <AppText style={styles.staNoteContent}>
             {statisticList.sumLength}km
           </AppText>
         </View>
       </View>
-      <View style={{borderBottomWidth: 0.5, borderBottomColor: 'black', width: 280, top: 25}}/>
-      <View style={{width: 300, height: 130, top: 25, flexDirection:'row',justifyContent:'space-evenly', alignItems: 'center'}}>
-        <View style={{backgroundColor: '#FFE99C', width: 80, height: 80, borderRadius: 50}}>
-          <FastImage source={require("@/assets/img_icon/shoe_icon.png")} style={{width:80, height:80}}/>
-        </View>
+      <View style={styles.staNoteLine}/>
+      <View style={styles.staNoteSection}>
+        <FastImage source={require("@/assets/img_icon/sand_clock_icon.png")} style={styles.staNoteIcon}/>
         <View>
-          <AppText style={{fontFamily: 'NPSfont_bold',fontSize: 30, color: 'black'}}>
-            이동한 거리
-          </AppText>
-          <AppText style={{fontFamily: 'NPSfont_bold',fontSize: 20, color: 'black'}}>
-            {statisticList.sumLength}km
-          </AppText>
-        </View>
-      </View>
-      <View style={{borderBottomWidth: 0.5, borderBottomColor: 'black', width: 280, top: 25}}/>
-      <View style={{width: 300, height: 130, top: 25, flexDirection:'row',justifyContent:'space-evenly', alignItems: 'center'}}>
-        <View style={{backgroundColor: '#FFE99C', width: 80, height: 80, borderRadius: 50}}>
-          <FastImage source={require("@/assets/img_icon/sand_clock_icon.png")} style={{width:80, height:80}}/>
-        </View>
-        <View>
-          <AppText style={{fontFamily: 'NPSfont_bold',fontSize: 30, color: 'black'}}>
+          <AppText style={styles.staNoteTitle}>
             이동한 시간
           </AppText>
-          <AppText style={{fontFamily: 'NPSfont_bold',fontSize: 20, color: 'black'}}>
+          <AppText style={styles.staNoteContent}>
             {statisticList.hour}시간 {statisticList.minute}분 {statisticList.second}초
           </AppText>
         </View>
       </View>
-      <View style={{borderBottomWidth: 0.5, borderBottomColor: 'black', width: 280, top: 25}}/>
-      <View style={{width: 300, height: 130, top: 25, flexDirection:'row',justifyContent:'space-evenly', alignItems: 'center'}}>
-        <View style={{backgroundColor: '#FFE99C', width: 80, height: 80, borderRadius: 50}}>
-          <FastImage source={require("@/assets/img_icon/trash_icon.png")} style={{width:80, height:80}}/>
-        </View>
+      <View style={styles.staNoteLine}/>
+      <View style={styles.staNoteSection}>
+        <FastImage source={require("@/assets/img_icon/trash_icon.png")} style={styles.staNoteIcon}/>
         <View>
-          <AppText style={{fontFamily: 'NPSfont_bold',fontSize: 30, color: 'black'}}>
+          <AppText style={styles.staNoteTitle}>
             쓰레기 개수
           </AppText>
-          <AppText style={{fontFamily: 'NPSfont_bold',fontSize: 20, color: 'black'}}>
+          <AppText style={styles.staNoteContent}>
             {statisticList.sumTrash}개
           </AppText>
         </View>
