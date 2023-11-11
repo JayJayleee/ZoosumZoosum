e25a3c7cacd7b8d0471578510fe9c017a49f4fb0
노수혁 <roh403@naver.com>
2023-11-11 18:05:38 +0900

    Style: 리스트 스타일 타입마다 변경되도록 코드 추가

--- frontend/src/components/ui/SelectList/index.tsx
+++ frontend/src/components/ui/SelectList/index.tsx
@@ -1,11 +1,16 @@
+import { windowHeight } from '@/constants/styles';
 import React, {useEffect} from 'react'
+import { StyleSheet } from 'react-native';
 import { SelectList, MultipleSelectList }from 'react-native-dropdown-select-list'
 
 interface singleProps {
   dataList: Array<string>;
-  setSelected: React.Dispatch<React.SetStateAction<string>>;
-  maxHeight: number;
-  placeholder: string;
+  setSelected: (e: string) => Promise<void> | React.Dispatch<React.SetStateAction<string>>;
+  maxHeight?: number | undefined;
+  placeholder?: string;
+  defalut?: string;
+  boxStyle?: 'defaultbox' | 'typebox';
+  listStyle?: 'defaultlist' | 'typelist'
 };
 
 interface InputSingleType {
@@ -13,7 +18,42 @@ interface InputSingleType {
   value: string;
 };
 
-export const SingleSelect = ({dataList, setSelected, maxHeight, placeholder}: singleProps) => {
+export const SingleSelect = ({
+    dataList, 
+    setSelected, 
+    maxHeight = undefined, 
+    placeholder, 
+    defalut = undefined,
+    boxStyle = 'defaultbox',
+    listStyle = 'defaultlist'
+  }: singleProps) => {
+
+  let variant;
+  let variantInput;
+  let variantStyle;
+  let variantText;
+
+  switch (boxStyle) {
+    case 'defaultbox':
+      variant = style.default;
+      variantInput = style.defaultInput;
+      break;
+    case 'typebox':
+      variant = style.type;
+      variantInput = style.typeInput;
+      break;
+  }
+
+  switch (listStyle) {
+    case 'defaultlist':
+      variantStyle = style.defaultlist;
+      variantText = style.defaultText;
+      break;
+    case 'typelist':
+      variantStyle = style.typelist;
+      variantText = style.typeText;
+      break;
+  }
 
   // 전달받은 데이터 리스트를 하나씩 대괄호로 리스트화하기 위해 빈 리스트 생성
   const data:Array<Object> = []
@@ -26,18 +66,63 @@ export const SingleSelect = ({dataList, setSelected, maxHeight, placeholder}: si
     data.push(tmp)
   })
 
+
   return (
     <SelectList
      setSelected={(val:string) => setSelected(val)} 
      data={data} 
-     maxHeight={maxHeight} 
+     maxHeight={maxHeight? maxHeight : undefined} 
      search={false}
-     boxStyles={{width: '80%', borderColor:"#6B4EFF", borderWidth: 2}} 
-     inputStyles={{width: '100%', fontSize: 18}}
-     dropdownStyles={{borderColor:"#6B4EFF", borderWidth: 2, width: 'auto'}}
-     dropdownTextStyles={{fontSize: 18, width:'80%'}}
+     boxStyles={variant} 
+     inputStyles={variantInput}
+     dropdownStyles={variantStyle}
+     dropdownTextStyles={variantText}
      fontFamily='NPSfont_regular'
-     placeholder={placeholder}
+     placeholder={placeholder? placeholder : undefined}
+     defaultOption={defalut? {key: defalut, value: defalut} : undefined}
      />
   )
 }
+
+const style = StyleSheet.create({
+  default: {
+    width: '80%', 
+    borderColor:"#6B4EFF", 
+    borderWidth: 2,
+  }, 
+  type: {
+    width: '45%', 
+    backgroundColor: '#A5DCA0',
+    borderColor: 'green',
+  },
+  defaultInput: {
+    width: '100%', 
+    fontSize: 18
+  }, 
+  typeInput: {
+    width: '100%', 
+    fontSize: 17,
+    fontFamily: 'NPSfont_bold'
+  },
+  defaultlist: {
+    borderColor:"#6B4EFF", 
+    borderWidth: 2, 
+    width: 'auto'
+  }, 
+  typelist: {
+    backgroundColor: '#A5DCA0',
+    borderColor: 'green',
+    borderWidth: 2, 
+    width: 'auto',
+    zIndex: 100,
+    height: windowHeight* 0.15
+  },
+  defaultText: {
+    fontSize: 18, 
+    width:'80%'
+  }, 
+  typeText: {
+    fontSize: 17, 
+    width:'100%'
+  },
+})
