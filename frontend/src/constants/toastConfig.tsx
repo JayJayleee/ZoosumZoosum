8e42f5c1307484779a692c0eb388d1f9373d5d3c
노수혁 <roh403@naver.com>
2023-11-11 15:56:42 +0900

    Feat: toast 설정 파일 constant로 이동

---
+++ frontend/src/constants/toastConfig.tsx
@@ -0,0 +1,25 @@
+import Toast, {BaseToast, ErrorToast} from "react-native-toast-message";
+
+export const toastConfig = {
+  success: (props: any) => (
+    <BaseToast
+      {...props}
+      style={{backgroundColor: '#34D399', borderLeftColor: '#34D399'}}
+      text1Style={{fontSize: 20, fontFamily: 'NPSfont_bold', color: 'white'}}
+    />
+  ),
+  error: (props: any) => (
+    <ErrorToast
+      {...props}
+      style={{backgroundColor: '#D94040', borderLeftColor: '#D94040'}}
+      text1Style={{fontSize: 20, fontFamily: 'NPSfont_bold', color: 'white'}}
+    />
+  ),
+  end: (props: any) => (
+    <BaseToast
+      {...props}
+      style={{backgroundColor: '#2C9261', borderLeftColor: '#2C9261', justifyContent: 'center', alignItems: 'center'}}
+      text1Style={{fontSize: 20, fontFamily: 'NPSfont_bold', color: 'white'}}
+    />
+  )
+}
