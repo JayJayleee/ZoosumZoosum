de4119c6c8334f2d6d623e42d99d1544566d4197
정규성 <aparter01@naver.com>
2023-11-14 13:57:22 +0900

    Feat: nms 순서에 다른 오류 수정

--- pythonBackend/fastapiserver.py
+++ pythonBackend/fastapiserver.py
@@ -100,60 +100,83 @@ def getResultFromData(
                 x1, y1, x2, y2 = bbox
 
                 if x1 < 0 or x2 > w or y1 < 0 or y2 > h :
-                    # print("이미지 바운더리 체크 필요")
+                    print("이미지 바운더리 체크 필요")
                     continue
 
                 # 바운더리 초과 시 결과에 포함하지 않음
                 if x1 <= x1Limit or y1 <= y1Limit or x2 >= x2Limit or y2 >= y2Limit:
-                    # print("이미지 바운더리 초과")
+                    print("이미지 바운더리 초과")
                     bigFlag = True
                     continue
                 
                 imageArea = (x2 - x1) * (y2 - y1)
                 if imageArea < minimumImageArea :
                     smallFlag = True
-                    # print("it's too small object")
-                    # print("minimumImageArea : ", minimumImageArea)
-                    # print("imageArea : ", imageArea)
+                    print("it's too small object")
+                    print("minimumImageArea : ", minimumImageArea)
+                    print("imageArea : ", imageArea)
                     continue
 
-                if box.cls == 0.:
-                    class_ids.append(0)
-                elif box.cls == 1. or box.cls == 2.:
-                    class_ids.append(1)
-                elif box.cls == 3.:
-                    class_ids.append(2)
-                elif box.cls == 4.:
-                    class_ids.append(3)
-                elif box.cls == 5.:
-                    class_ids.append(4)
-                elif box.cls == 7.:
-                    class_ids.append(5)
-                else :
-                    continue
+                
+                
+
+                # # print(box.cls)
+
+                # if box.cls == 0.:
+                #     class_ids.append(0)
+                #     # trash_count[0] += 1
+                # elif box.cls == 1. or box.cls == 2.:
+                #     class_ids.append(1)
+                #     # trash_count[1] += 1
+                # elif box.cls == 3.:
+                #     class_ids.append(2)
+                #     # trash_count[2] += 1
+                # elif box.cls == 4.:
+                #     class_ids.append(3)
+                #     # trash_count[3] += 1
+                # elif box.cls == 5.:
+                #     class_ids.append(4)
+                #     # trash_count[4] += 1
+                # elif box.cls == 7.:
+                #     class_ids.append(5)
+                #     # trash_count[5] += 1
+                # else :
+                #     continue
 
                 bboxes.append(xyxy)
                 confidences.append(float(confidence))
     # cv2 가 제공하는 후처리 모델
     result_boxes = cv2.dnn.NMSBoxes(bboxes, confidences, 0.25, 0.45, 0.5)
 
+    newBoxes = []
+
     for result in results:
         boxes = result.boxes
         # for box in boxes:
         for i in range(len(bboxes)):
             if i in result_boxes:
+
                 box = boxes[i]
+                xyxy = box.xyxy.tolist()[0]
+                newBoxes.append(xyxy)
+
                 if box.cls == 0.:
+                    class_ids.append(0)
                     trash_count[0] += 1
                 elif box.cls == 1. or box.cls == 2.:
+                    class_ids.append(1)
                     trash_count[1] += 1
                 elif box.cls == 3.:
+                    class_ids.append(2)
                     trash_count[2] += 1
                 elif box.cls == 4.:
+                    class_ids.append(3)
                     trash_count[3] += 1
                 elif box.cls == 5.:
+                    class_ids.append(4)
                     trash_count[4] += 1
                 elif box.cls == 7.:
+                    class_ids.append(5)
                     trash_count[5] += 1
                 else :
                     continue
@@ -173,7 +196,7 @@ def getResultFromData(
     result = {
         "predict_result" : predict_result,
         "confidences" : confidences,
-        "bboxes" : bboxes,
+        "newBoxes" : newBoxes,
         "class_ids" : class_ids,
         "img" : img,
         "nparr" : nparr,
@@ -211,19 +234,19 @@ def predict(
     bottomPercentPercent : Optional[float] = Form(0.2),
     confidenceValue : Optional[float] = Form(0.5),
     modelType : Optional[str] = Form('s'),
-    imageDetectAreaRatio : Optional[float] = Form(0.05),
+    imageDetectAreaRatio : Optional[float] = Form(0.03),
     ):
     
     result = getResultFromData(file, leftRightPercent, topPercentPercent,bottomPercentPercent, confidenceValue, modelType, imageDetectAreaRatio)
     predict_result = result["predict_result"]
     confidences = result["confidences"]
-    bboxes = result["bboxes"]
+    newBoxes = result["newBoxes"]
     class_ids = result["class_ids"]
     img = result["img"]
     nparr = result["nparr"]
 
     # cv2 가 제공하는 후처리 모델
-    result_boxes = cv2.dnn.NMSBoxes(bboxes, confidences, 0.25, 0.45, 0.5)
+    # result_boxes = cv2.dnn.NMSBoxes(bboxes, confidences, 0.25, 0.45, 0.5)
 
     
     # print(result_boxes)
@@ -233,9 +256,9 @@ def predict(
     font = cv2.FONT_HERSHEY_PLAIN
 
     # cv2 박스, 글씨 생성
-    for i in range(len(bboxes)):
-        if i in result_boxes:
-            bbox = list(map(int, bboxes[i])) 
+    for i in range(len(newBoxes)):
+        # if i in result_boxes:
+            bbox = list(map(int, newBoxes[i])) 
             x, y, x2, y2 = bbox
 
             cv2.rectangle(img, (x, y), (x2, y2), color[class_ids[i]], 14)
