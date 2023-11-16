bcf9bb98facef7c5a9f9fb2d9f6581f51f617d23
thdqudgns <thdqudgns9612@gmail.com>
2023-11-16 22:50:58 +0900

    Fix: fe remove

--- frontend/src/apis/tree.ts
+++
@@ -1,6 +0,0 @@
-import {api, Header} from './index';
-import {tree} from '@/types/tree';
-
-export const treeApi = async (data: tree) => {
-  return await api.post('/userinfo/tree', data, await Header());
-};
--- frontend/src/types/tree.ts
+++
@@ -1,6 +0,0 @@
-export type tree = {
-  userName: string;
-  treeName: string;
-  userPhone: string | null;
-  userBirth: string | null;
-};
